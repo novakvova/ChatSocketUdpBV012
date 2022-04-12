b9ed3c60fca367d2bb2c430650ea43b2fa92bdb1
novakvova <novakvova@gmail.com>
2022-04-12 19:54:27 +0300

    Begin server chat

---
+++ ChatLib/Paket.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatLib
+{
+    public enum DataIdentifier
+    {
+        Message,
+        LogIn,
+        LogOut,
+        Null
+    }
+
+    public class Paket
+    {
+        public DataIdentifier TypeMessage { get; set; }
+        public string UserName { get; set; }
+        public string Message { get; set; }
+
+        public Paket() { }
+
+        /// <summary>
+        /// Отримання даних від клієнта, розбираємо байти
+        /// </summary>
+        /// <param name="dataStream">масив байтів</param>
+        public Paket(byte [] dataStream)
+        {
+            TypeMessage = (DataIdentifier)BitConverter.ToInt32(dataStream, 0);
+            //довжина імені користувача
+            int nameLength = BitConverter.ToInt32(dataStream, 4);
+            //довжина повідомлення
+            int msgLength = BitConverter.ToInt32(dataStream, 8);
+            if (nameLength > 0)
+                UserName = Encoding.UTF8.GetString(dataStream, 12, nameLength);
+            else
+                UserName = null;
+
+            if (msgLength > 0)
+                Message = Encoding.UTF8.GetString(dataStream, 12+nameLength, msgLength);
+            else
+                Message = null;
+        }
+
+        public byte[] GetDataStream()
+        {
+            List<byte> dataStream = new List<byte>();
+
+            // Add the dataIdentifier
+            dataStream.AddRange(BitConverter.GetBytes((int)TypeMessage));
+
+            // Add the name length
+            if (UserName != null)
+                dataStream.AddRange(BitConverter.GetBytes(Encoding.UTF8.GetBytes(UserName).Length));
+            else
+                dataStream.AddRange(BitConverter.GetBytes(0));
+
+            // Add the message length
+            if (Message != null)
+                dataStream.AddRange(BitConverter.GetBytes(Encoding.UTF8.GetBytes(Message).Length));
+            else
+                dataStream.AddRange(BitConverter.GetBytes(0));
+
+            // Add the name
+            if (UserName != null)
+                dataStream.AddRange(Encoding.UTF8.GetBytes(UserName));
+
+            // Add the message
+            if (Message != null)
+                dataStream.AddRange(Encoding.UTF8.GetBytes(Message));
+
+            return dataStream.ToArray();
+        }
+
+    }
+}
---
+++ ServerChatConsole/Program.cs
@@ -0,0 +1,66 @@
+﻿using ChatLib;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace ServerChatConsole
+{
+    class Program
+    {
+        
+
+        private struct Client
+        {
+            public EndPoint endPoint;
+            public string name;
+        }
+        List<Client> clients = new List<Client>();
+
+        // Server socket
+        private static Socket serverSocket;
+
+        // Data stream
+        private static byte[] dataStream = new byte[1024];
+        public static ManualResetEvent allDone = new ManualResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Encoding.UTF8.GetBytes("vvПривіт").Length);
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint endPoint = new IPEndPoint(ip, 2000);
+            serverSocket.Bind(endPoint);
+
+            Console.WriteLine("Server Start --------");
+            while(true)
+            {
+                allDone.Reset();
+                IPEndPoint client = new IPEndPoint(ip, 0);
+                EndPoint epSender = (EndPoint)client;
+                serverSocket.BeginReceiveFrom(dataStream, 0, dataStream.Length, SocketFlags.None, ref epSender,
+                    new AsyncCallback(ReceiveData), epSender);
+                allDone.WaitOne();
+            }
+            
+            //Console.ReadKey();
+        }
+
+        private static void ReceiveData(IAsyncResult asyncResult)
+        {
+            try
+            {
+                Paket clientData = new Paket(dataStream);
+                allDone.Set();
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Problem read data");
+            }
+        }
+    }
+}
